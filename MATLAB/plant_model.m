503c8216d4bf7b800dd718a416a9a2d8a6bf79e2
ParkerEnsing <parker.a.ensing@gmail.com>
2024-05-31 19:23:22 -0400

    Changed plant model to undamped inverted pendulum

--- MATLAB/plant_model.m
+++ MATLAB/plant_model.m
@@ -1,16 +1,14 @@
 close all; clear; clc;
 
-t = 0:0.01:10; % time (s)
+t = 0:0.01:1; % time (s)
 
-% Dynamics for a undamped pendulum (non-inverted)
+% Dynamics for a undamped inverted pendulum
 
 theta_initial = 0.1; % angle from vertical (rad)
-theta_dot_initial = 0;
 g = 9.81; % gravitational acceleration (m/s/s)
-l = 0.1; % length of rod (m)
-k = sqrt(2*g / l);
-c2 = theta_dot_initial / k;
+l = 0.2; % length of rod (m)
+k = sqrt(g / l);
+c = theta_initial / 2;
 
-theta = @(t, k, c1, c2) c1*cos(k*t) + c2*sin(k*t);
-
-plot(t, theta(t, k, theta_initial, c2))
+theta = @(t, k, c) c*exp(k*t) + c*exp(-k*t);
+plot(t, theta(t, k, c))
